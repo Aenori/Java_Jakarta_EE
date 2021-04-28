a7ada68e497a65a874b33a2c00b5c001c8f14b28
Nicolas <Nicolas Rousset>
2021-04-28 11:02:38 +0200

    Testing loop

--- .github/scripts/run_tomcat.sh
+++ .github/scripts/run_tomcat.sh
@@ -8,12 +8,12 @@ do
 	tomcat_is_running=$(netstat -a | grep 8080)
 	echo "Test "$tomcat_is_running
 
-	if [ -n "$tomcat_is_running"]
+	if [ -n "$tomcat_is_running" ]
 	then
 		break
 	fi
 
-	if [i -eq 15]
+	if [ i -eq 15 ]
 	then
 		echo "ERROR : Tomcat didn't start in 15 seconds"
 		exit 1

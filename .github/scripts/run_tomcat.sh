e10229b82bab2ca337040f817d78de8797587220
Nicolas <Nicolas Rousset>
2021-04-28 10:50:25 +0200

    Preventing infinity loop in test Tomcat

--- .github/scripts/run_tomcat.sh
+++ .github/scripts/run_tomcat.sh
@@ -2,9 +2,17 @@
 set -euxo pipefail
 
 bash /opt/tomcat/bin/catalina.sh run&
+cpt=0
 while [ -z "$(netstat -a | grep 8080)"]
 do
 	sleep 1
+	((cpt+=1))
+
+	if ((cpt > 15))
+	then
+		echo "ERROR : Tomcat didn't start in 15 seconds"
+		exit(1)
+	fi
 done
 # Check tomcat is running
 curl http://localhost:8080 1> /dev/null

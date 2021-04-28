266bd6ea662d3be56e305c331d7e505b265acf35
Nicolas <Nicolas Rousset>
2021-04-28 10:47:44 +0200

    WiP

--- .github/scripts/run_tomcat.sh
+++ .github/scripts/run_tomcat.sh
@@ -2,5 +2,9 @@
 set -euxo pipefail
 
 bash /opt/tomcat/bin/catalina.sh run&
+while [ -z "$(netstat -a | grep 8080)"]
+do
+	sleep 1
+done
 # Check tomcat is running
 curl http://localhost:8080 1> /dev/null

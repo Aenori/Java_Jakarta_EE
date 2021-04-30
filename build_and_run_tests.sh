d68496f41256d56a72696c2f8fa720930c54df90
Nicolas <Nicolas Rousset>
2021-04-30 17:05:34 +0200

    WiP

--- build_and_run_tests.sh
+++ build_and_run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build . -t jakartaee
-docker run jakartaee bash -c "bash test_as_shell/run_tomcat.sh > /dev/null && bash test_as_shell/test_tp1.sh"
+docker run --name jakartaee_test jakartaee bash -c "bash test_as_shell/run_tomcat.sh > /dev/null && bash test_as_shell/test_tp1.sh"
+docker container rm jakartaee_test

b29ea89688237fab7f809ade5051946a39100469
Nicolas <Nicolas Rousset>
2021-04-30 14:48:08 +0200

    Exercice 3 and test case Ok !

--- test_as_shell/test_exercice_2.sh
+++ test_as_shell/test_exercice_2.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 set -e
 
+source test_as_shell/helper_function_test.sh
+
 result=$(curl 'http://localhost:8080/java-web-project/FormPrenomNom' 2>/dev/null)
 result_trimmed=$(echo $result | tr -d ' ')
 
-if [ -z "$(echo $result_trimmed | grep 'action=\"Bonjour\"')" ]
-then
-	echo "ERROR : FormPrenomPrenom should contain an action redirecting to Bonjour"
-	echo $result
-  exit 1
-elif [ -z "$(echo $result_trimmed | grep 'type=\"submit\"')" ]
-then
-	echo "ERROR : FormPrenomPrenom should contain a submit button"
-	echo $result
-	exit 1
-elif [ -z "$(echo $result_trimmed | grep 'name=\"nom\"')" ]
-then
-	echo "ERROR : FormPrenomPrenom should contain an input field with name nom"
-	echo $result
-	exit 1
-elif [ -z "$(echo $result_trimmed | grep 'name=\"prenom\"')" ]
-then
-	echo "ERROR : FormPrenomPrenom should contain an input field with name prenom"
-	echo $result
-	exit 1
-fi
+test_form_page "$testname" "$result_trimmed" "Bonjour"
+test_form_input "$testname" "$result_trimmed" "prenom"
+test_form_input "$testname" "$result_trimmed" "nom"
 
 echo "Exercice n 2 : OK"
